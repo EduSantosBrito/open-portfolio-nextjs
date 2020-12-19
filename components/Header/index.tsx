b6a82fc642ab5370b0719da28d370f7b479008b6
EduSantosBrito <edu.santos.brito@gmail.com>
2020-12-19 02:09:52 -0300

    chore: create mobile menu and hover effect in nav item

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,10 @@
+import NavItem from '@components/NavItem';
 import { useTheme } from '@contexts/ThemeContext';
 import darkModeStyle from '@styles/components/Header/dark.module.scss';
 import defaultStyle from '@styles/components/Header/default.module.scss';
 import lightModeStyle from '@styles/components/Header/light.module.scss';
 import { mergeStyles } from '@utils/mergeStyles';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 const themes = {
   DARK: darkModeStyle,
@@ -12,6 +13,7 @@ const themes = {
 
 const Header = (): JSX.Element => {
   const { theme, switchTheme } = useTheme();
+  const [open, setOpen] = useState(false);
 
   const styles = useMemo(() => {
     const themeStyle = themes[theme];
@@ -20,16 +22,37 @@ const Header = (): JSX.Element => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Daibert</div>
-      <div className={styles.nav}>
-        <div className={styles.nav_item}>
-          <button onClick={() => switchTheme()}>switch</button>
+      <div className={`${styles.content} ${open ? styles.open : ''}`}>
+        <button
+          onClick={() => setOpen(!open)}
+          className={`${styles.icon} ${open ? styles.open : ''}`}
+        />
+        <img
+          className={`${styles.logo} ${open ? styles.open : ''}`}
+          alt='Logo description'
+        />
+        <div className={`${styles.iconCTA} ${open ? styles.open : ''}`}>
+          icon2
         </div>
-        <div className={styles.nav_item}>Home</div>
-        <div className={styles.nav_item}>Sobre mim</div>
-        <div className={styles.nav_item}>Projetos</div>
-        <div className={styles.nav_item}>
-          <button>Orcamento</button>
+        <div className={`${styles.nav} ${open ? styles.open : ''}`}>
+          <NavItem
+            label='switch'
+            render={
+              <button
+                className={`${styles.buttonTheme} ${open ? styles.open : ''}`}
+                onClick={() => switchTheme()}
+              >
+                switch
+              </button>
+            }
+          />
+          <NavItem label='Home' to='#' />
+          <NavItem label='Sobre mim' to='#about' />
+          <NavItem label='Projetos' to='#projects' />
+          <NavItem
+            label='Orçamento'
+            render={<button className={styles.buttonCTA}>Orcamento</button>}
+          />
         </div>
       </div>
     </div>
